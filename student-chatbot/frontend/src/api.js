af5f94f36f12d9359ea12b881a0dd708b1eef33d
tarunpratap1 <tpwonderno1@gmail.com>
2026-01-04 10:51:44 +0530

    Update api.js

--- student-chatbot/frontend/src/api.js
+++ student-chatbot/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://student-chatbot-2-4m28.onrender.com',
+  baseURL: 'https://student-chatbot-2-4m28.onrender.com/api',
 });
 
 export const setToken = (token) => {
